af35a205701b5e5a6f4e5bea4c0d3f879d7dad7a
Patrick Nick <patrick.dale.nick@gmail.com>
2015-10-28 23:40:34 +0100

    - add project status dropdown - various interface changes

--- app/requests/requests.js
+++ app/requests/requests.js
@@ -4,11 +4,30 @@ angular.module('wtt.requests', ['ui.materialize'])
 
     .controller('RequestsCtrl', function ($scope, $state, $http, $location) {
 
-        $scope.requestData = [];
+        $scope.projectStatus = [];
+
+        $scope.$on('ngSelectRepeatFinished', function (ngRepeatFinishedEvent) {
+            //$('[material-select]').material_select('destroy');
+            $('[material-select]').material_select();
+        });
 
         $scope.init = function () {
-            $('.button-collapse').sideNav();
-        }
+            $.get('/api/1.0/projectstatus').success(function(data){
+                $scope.projectStatus = data;
+            });
+        };
 
         $scope.init();
+    })
+    .directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit('ngSelectRepeatFinished');
+                    });
+                }
+            }
+        }
     });
--- app/requests/results/results.js
+++ app/requests/results/results.js
@@ -5,14 +5,64 @@
 
 angular.module('wtt.results', [])
 
-    .controller('ResultsCtrl', function($scope,$http,$location,$state) {
+    .controller('ResultsCtrl', function ($scope, $http, $location, $state) {
         $scope.requestData = [];
+        $scope.requests = [];
 
-        $scope.init = function(){
-            $http.get('/api/1.0/orders').success(function (data) {
+        $scope.init = function () {
+            $scope.loadRequests('/api/1.0/orders');
+        }
+
+        $scope.loadRequests = function (url) {
+            $http.get(url).success(function (data) {
                 $scope.requestData = data;
+
+                if (data != null) {
+                    $scope.requests.push(data.data);
+
+                    for (var index in $scope.requestData.data) {
+                        var request = $scope.requestData.data[index];
+                        if (request.sadDate !== null) {
+                            request.sadDate.date = new Date(request.sadDate.date);
+                        }
+                        if (request.currentWorkflowState.currentState === -1) {
+                            request.currentWorkflowState.stateText = "Not available";
+                        } else {
+                            request.currentWorkflowState.stateText = request.availableMilestones[request.currentWorkflowState.currentState];
+                        }
+                    }
+                }
             });
         }
 
+        $scope.loadMoreRequests = function(){
+            $scope.loadRequests($scope.requestData.next_page_url);
+        }
+
+        $scope.$on('ngRepeatFinished', function (ngRepeatFinishedEvent) {
+            Materialize.showStaggeredList('#results');
+        });
+
+        var options = [
+            {selector: '#test', offset: -400, callback: function(){
+                $scope.loadRequests($scope.requestData.next_page_url);
+            }}
+        ];
+
+        Materialize.scrollFire(options);
+
         $scope.init();
+    })
+
+    .directive('onFinishRender', function ($timeout) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attr) {
+                if (scope.$last === true) {
+                    $timeout(function () {
+                        scope.$emit('ngRepeatFinished');
+                    });
+                }
+            }
+        }
     });
